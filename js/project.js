f8068ebae8f05b0037a605c9c757ebb1f7e6fae8
Joey Lee <eli052@aucklanduni.ac.nz>
2020-06-11 04:39:20 +1200

    updated js file

--- js/project.js
+++ js/project.js
@@ -4,66 +4,78 @@ const PROGRAMS = [
 		program_title: "Volunteer exchange program",
 		program_subtitle: "If you are between 18 to 25",
     program_description: "Work, study and travel with our Volunteer exchange program.",
+		more: "more"
 	},
 	{
     id: "program-2",
 		program_title: "Gap year exchange program",
 		program_subtitle: "If you are in your gap year",
     program_description: "Spend your gap year with our Gap year exchange program",
-
+		more: "more"
 	},
 	{
     id: "program-3",
 		program_title: "University student exchange program",
 		program_subtitle: "If you are an university student",
     program_description: "Study in different country with our University student exchange program",
-
+		more: "more"
 	},
 	{
     id: "program-4",
 		program_title: "Entrepreneur exchange programe",
 		program_subtitle: "If you just finished your degree",
     program_description: "Gain work experience with our Entrepreneur exchange program",
-
+		more: "more"
 	},
 	{
     id: "program-5",
 		program_title: "Company team exchange program",
 		program_subtitle: "If you are in a crew",
     program_description: "Bring your team with you with our Company team exchange program",
-
+		more: "more"
 	},
  ];
 
- function createProgramHTML(program) {
 
- 	let programHTML = $("<article>");
 
- 	let title = $("<h2>" + program.program_title + "</h2>");
- 	programHTML.append(title);
 
- 	let subtitle = $("<h3>" + program.program_subtitle + "</h3>");
- 	programHTML.append(subtitle);
+$(document).ready(function(){
+	const HTML_PREFIX = "html";
+	const DESCRIPTION_PREFIX = "description";
 
- 	let description = $("<p>" + program.program_description + "</p>");
- 	programHTML.append(description);
+	$.each(PROGRAMS,function(index, val) {
+		let programHTML = $("<article>")
+			.attr("id", HTML_PREFIX + index);
 
- 	return programHTML;
- }
+	 	let title = $("<h2>" + val.program_title + "</h2>");
+	 	programHTML.append(title);
 
- function displayPrograms(programs) {
- 	let programsList = $("#programList");
+	 	let subtitle = $("<h3>" + val.program_subtitle + "</h3>");
+	 	programHTML.append(subtitle);
 
- 	programsList.empty();
- 	for(let program of programs) {
- 		let programHTML = createProgramHTML(program);
- 		programsList.append(programHTML);
- 	}
- }
+		let more = $("<p>" + val.more + "</p>");
+		programHTML.append(more);
 
- $(document).ready(function(){
- 	displayPrograms(PROGRAMS);
- });
+	 	let description = $("<p>")
+			.text(val.program_description)
+			.attr("id", DESCRIPTION_PREFIX + "_" + HTML_PREFIX + index)
+	    .addClass("hidden")
+
+	 	programHTML.append(description);
+
+		$('#programList').append(programHTML);
+
+		programHTML.on("click", displayPrograms);
+
+	});
+
+	function displayPrograms() {
+		let programHTML = $(this);
+	  let detailsHTMLID = DESCRIPTION_PREFIX + "_" + programHTML.attr('id');
+
+	  $('#' + detailsHTMLID).toggle(200);
+	}
+});
 
 
 
